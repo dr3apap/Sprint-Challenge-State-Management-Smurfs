cc64f1ff671d061dd8c17b575d25da979b31d1ec
Adebola  Adesina <dreapap@gmail.com>
2020-02-21 15:23:47 -0800

    setup complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import "./App.css";
+import { SmurfProvider } from "../SmurffyContext/SmurfProvider";
+import AddSmurfs from "../SmurffyContext/AddSmurfs";
+import SmurfList from "../SmurffyContext/SmurfList";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <div className='App'>
+        {/* <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div> */}
+        <SmurfProvider>
+          <AddSmurfs />
+          <SmurfList />
+        </SmurfProvider>
+        {/* <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div> */}
       </div>
     );
   }
